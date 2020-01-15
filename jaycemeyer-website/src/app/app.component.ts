58aabaaf24a6fe493a62e3d07c26bf55ca79ef3a
Jayce <jmeyer@vertafore.com>
2020-01-15 16:09:36 -0700

    Add page components, remove spec files, toolbar styling

--- jaycemeyer-website/src/app/app.component.ts
+++ jaycemeyer-website/src/app/app.component.ts
@@ -5,6 +5,47 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'jaycemeyer-website';
+  date = new Date().getFullYear();
+  displayAboutPage: boolean = true;
+  displayPortfolioPage: boolean = false;
+  displayContactPage: boolean = false;
+  displayResumePage: boolean = false;
+
+  loadPage(pageName: string) {
+    switch(pageName) {
+      case "about": {
+        this.displayAboutPage = true;
+        this.displayPortfolioPage = false;
+        this.displayContactPage = false;
+        this.displayResumePage = false;
+        break;
+      }
+      case "portfolio": {
+        this.displayAboutPage = false;
+        this.displayPortfolioPage = true;
+        this.displayContactPage = false;
+        this.displayResumePage = false;
+        break;
+      }
+      case "contact": {
+        this.displayAboutPage = false;
+        this.displayPortfolioPage = false;
+        this.displayContactPage = true;
+        this.displayResumePage = false;
+        break;
+      }
+      case 'resume': {
+        this.displayAboutPage = false;
+        this.displayPortfolioPage = false;
+        this.displayContactPage = false;
+        this.displayResumePage = true;
+        break;
+      }
+    }
+    
+    
+  }
 }
--- jaycemeyer-website/src/app/app.module.ts
+++ jaycemeyer-website/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ResumePageComponent } from './resume-page/resume-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent,
+    PortfolioPageComponent,
+    ContactPageComponent,
+    ResumePageComponent
   ],
   imports: [
     BrowserModule
